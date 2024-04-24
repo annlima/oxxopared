db1d6b2966ceb08c7d1cf8bd1828472ea3ef9de1
azu-any <azuany.mila@gmail.com>
2024-04-23 21:23:32 -0600

    spots examples

--- oxxopared/Spot/Spot.swift
+++ oxxopared/Spot/Spot.swift
@@ -12,8 +12,35 @@ struct Spot {
     let title: String
     let image: Image?
     let text: String
-    let category: String
+    let category: String // oportunidades, servicios, artículos de segunda mano, cosas perdidas
 }
 
 extension Spot {
+    
+    static var spot1: Spot {
+        Spot(
+            title: "Busco a mi gato Mimmie",
+            image: Image(.michi),
+            text: "Perdí mi gato en la Av. Eugenio Garza Sada. Cualquier información me ayudaría 🙏. Contaáctame al 0123456789",
+            category: "Cosas perdidas"
+        )
+    }
+    
+    static var spot2: Spot {
+        Spot(
+            title: "Ofrezco clases de guitatarra",
+            image: nil,
+            text: "Doy clases de guitarra a principiantos. Tengo múltiples referencias. Más información al 9876543210",
+            category: "Servicios"
+        )
+    }
+    
+    static var spot3: Spot {
+        Spot(
+            title: "Se solicita cajero en Oxxo",
+            image: nil,
+            text: "Trabajo de medio tiempo en Oxxo de Garza Sada. Sexo indifirente, mínimo 18 años con prepa terminada. Para más información contactar al 2222222222",
+            category: "Servicios"
+        )
+    }
 }
