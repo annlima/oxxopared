b3b1137cb5bac53007908802e51de5fc7b0e38aa
Fer Ahuatzin <64028335+FerAhuatzin@users.noreply.github.com>
2024-04-23 21:24:45 -0600

    Feed wiew

---
+++ oxxopared/Feed/Feed.swift
@@ -0,0 +1,18 @@
+//
+//  Feed.swift
+//  oxxopared
+//
+//  Created by iOS Lab on 23/04/24.
+//
+
+import SwiftUI
+
+struct Feed: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Feed()
+}
