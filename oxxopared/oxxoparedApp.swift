c1eca26680c75ba5e3385e60436a0740fd4ceaf9
Andrea Lima <89811870+annlima@users.noreply.github.com>
2024-04-23 19:05:24 -0600

    Initial Commit

---
+++ oxxopared/oxxoparedApp.swift
@@ -0,0 +1,17 @@
+//
+//  oxxoparedApp.swift
+//  oxxopared
+//
+//  Created by Andrea Lima Blanca on 23/04/24.
+//
+
+import SwiftUI
+
+@main
+struct oxxoparedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
