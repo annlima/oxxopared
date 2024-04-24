4376e64817dde307189a086d1fd0a5877c5034c9
Dicka Lezama <dj_lezama@hotmail.com>
2024-04-24 04:17:14 -0600

    AR View
    AR View (Azu :))

---
+++ oxxopared/Spot/SpotARView.swift
@@ -0,0 +1,18 @@
+//
+//  SpotARView.swift
+//  oxxopared
+//
+//  Created by Dicka J. Lezama on 24/04/24.
+//
+
+import SwiftUI
+
+struct SpotARView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SpotARView()
+}
